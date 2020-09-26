bc7d749b8e51722904814d94b8c6ca0378fa4729
vraxzeztan <anusarsoni88@gmail.com>
2020-09-26 22:14:38 +0530

    fixed date format

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -9,8 +9,8 @@ const ProfileEducation = ({
   <div>
     <h3 className="text-dark">{school}</h3>
     <p>
-      <Moment format="YYYY/MM/DD">{moment.utc(from)}</Moment> -{" "}
-      {!to ? " Now" : <Moment format="YYYY/MM/DD">{moment.utc(to)}</Moment>}
+      <Moment format="DD/MM/YYYY">{moment.utc(from)}</Moment> -{" "}
+      {!to ? " Now" : <Moment format="DD/MM/YYYY">{moment.utc(to)}</Moment>}
     </p>
     <p>
       <strong>Degree: </strong> {degree}
